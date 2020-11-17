2742f3fc488ea40f398e3f40f5544047eef8c7b0
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-17 21:10:38 +0200

    Update animate.js

--- JS/animate.js
+++ JS/animate.js
@@ -2,56 +2,56 @@ window.sr = ScrollReveal();
 
 sr.reveal('.animate-left',{
     origin: 'left',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 1000
+    delay: 500
 });
 
 sr.reveal('.animate-right',{
     origin: 'right',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 1000
+    delay: 500
 });
 
 sr.reveal('.animate-top',{
     origin: 'top',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 1000
+    delay: 500
 });
 
 sr.reveal('.animate-bottom',{
     origin: 'bottom',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 1000
+    delay: 500
 });
 
 sr.reveal('.animate-left-lazy',{
     origin: 'left',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 2500
+    delay: 1500
 });
 
 sr.reveal('.animate-right-lazy',{
     origin: 'right',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 2500
+    delay: 1500
 });
 
 sr.reveal('.animate-top-lazy',{
     origin: 'top',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 2500
+    delay: 1500
 });
 
 sr.reveal('.animate-bottom-lazy',{
     origin: 'bottom',
-    duration: 1000,
+    duration: 750,
     distance: '10rem',
-    delay: 2500
+    delay: 1500
 });
