2066fb11a1240324bce43012a52f32c6919d8474
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-18 23:34:54 +0200

    Update style.js

--- js/style.js
+++ js/style.js
@@ -97,3 +97,27 @@ grayBtn.onclick = function(){
     document.body.classList.remove("blue-mod", "red-mod", "green-mod")
     document.body.classList.add("gray-mod")
 }
+
+
+
+
+function getRandomRGBValue() {
+    return Math.min(Math.floor(Math.random() * 255 + 1), 255);
+}
+
+function getRandomColor() {
+    var r = getRandomRGBValue(),
+        g = getRandomRGBValue(),
+        b = getRandomRGBValue();
+    return "#" + (((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1));
+}
+
+function changeThemeColor() {
+    var metaThemeColor = document.querySelector("meta[name=theme-color]");
+    metaThemeColor.setAttribute("content", getRandomColor());
+    setTimeout(function() {
+        changeThemeColor();
+    }, 3000);
+}
+
+changeThemeColor();
