aec4e3c530f823c96c8ea50c7a7e588345bef600
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-18 23:43:57 +0200

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,5 +1,6 @@
 var myNav = document.getElementById('mynav'),
     mybutton = document.getElementById("myBtn"),
+    metaThemeColor = document.querySelector("meta[name=theme-color]"),
     modeBtn = document.querySelector(".colors");
     
 
@@ -76,8 +77,9 @@ var settings = document.querySelector(".settings"),
 
 blueBtn.onclick = function(){
     "use strict";
-    document.body.classList.remove("red-mod", "green-mod", "gray-mod")
-    document.body.classList.add("blue-mod")
+    document.body.classList.remove("red-mod", "green-mod", "gray-mod");
+    document.body.classList.add("blue-mod");
+    metaThemeColor.setAttribute("content","#000");
 }
 
 redBtn.onclick = function(){
@@ -101,23 +103,14 @@ grayBtn.onclick = function(){
 
 
 
-function getRandomRGBValue() {
-    return Math.min(Math.floor(Math.random() * 255 + 1), 255);
-}
 
-function getRandomColor() {
-    var r = getRandomRGBValue(),
-        g = getRandomRGBValue(),
-        b = getRandomRGBValue();
-    return "#" + (((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1));
+function getPresentColor() {
+    
 }
 
 function changeThemeColor() {
     var metaThemeColor = document.querySelector("meta[name=theme-color]");
-    metaThemeColor.setAttribute("content", getRandomColor());
-    setTimeout(function() {
-        changeThemeColor();
-    }, 3000);
+    metaThemeColor.setAttribute("content", getPresentColor());
 }
 
 changeThemeColor();
