de31249bb8409ab84779d5ae8c3ccbae8e942627
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-20 23:53:35 +0200

    preload done

--- js/preload.js
+++ js/preload.js
@@ -1,7 +1,11 @@
-// var metaThemeColor = document.querySelector("meta[name=theme-color]");
+var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
-// $(document).ready(function(){
-//     setTimeout(function(){
-//         $('loader-wrapper').addClass('test');},1000);
-//         metaThemeColor.setAttribute("content","#004A6B");
-//     });
+$(document).ready(function(){
+    "use strict";
+    metaThemeColor.setAttribute("content","#004A6B");
+});
+
+$(window).on('load', function(){
+    "use strict";
+    $("body").addClass("loaded");
+});
