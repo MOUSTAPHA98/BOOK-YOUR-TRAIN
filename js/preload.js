22198591fbea9147f3b3623a6d2af8b223116c7f
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-23 03:53:18 +0200

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,4 @@
-$(window).on('load', function(){
+$(window).on('preload', function(){
     "use strict";
     $("body").addClass("loaded");
     metaThemeColor.setAttribute("content", localStorage.getItem("pageTheme"));
