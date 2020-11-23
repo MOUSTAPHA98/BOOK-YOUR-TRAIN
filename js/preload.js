e66271bd2ca21b5a708bf9dad30847842a8e266d
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-23 03:51:39 +0200

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -1,12 +1,11 @@
-// var metaThemeColor = document.querySelector("meta[name=theme-color]");
-
 $(document).ready(function(){
     "use strict";
     metaThemeColor.setAttribute("content", localStorage.getItem("pageTheme"));
-
 });
 
 $(window).on('load', function(){
     "use strict";
     $("body").addClass("loaded");
+    metaThemeColor.setAttribute("content", localStorage.getItem("pageTheme"));
+
 });
