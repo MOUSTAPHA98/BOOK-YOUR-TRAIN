9f1dc6b57ee7c8f60b4eef7dfff6ad179edb2b87
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-11-30 20:26:07 +0200

    .

--- js/style.js
+++ js/style.js
@@ -9,18 +9,17 @@ var myNav = document.getElementById('mynav'),
     mobRImgS  = document.querySelector(".mob-r-img-s"),
     mobGreenImgS  = document.querySelector(".mob-green-img-s"),
     mobGrayImgS  = document.querySelector(".mob-gray-img-s"),
-    modeBtn = document.querySelector(".colors");
+    modeBtn = document.querySelector(".themes");
 
 
 
 
 window.onscroll = function () { 
-    "use strict";
     if (document.body.scrollTop >= 700 || document.documentElement.scrollTop >= 700 )
     {
         myNav.classList.add("nav-colored");
         modeBtn.style.display = "none";
-         myNav.classList.remove("nav-transparent");
+        myNav.classList.remove("nav-transparent");
         modeBtn.style.display = "none";
 
     } 
@@ -54,14 +53,12 @@ function topFunction() {
 var menuOpenBtn = document.getElementById("fa-bars"),
     menu = document.getElementById("menu");
         menuOpenBtn.onclick = function(){
-            "use strict";
             menu.classList.add("expanded")
         }
 
 /* CLOSE */
 var menuCloseBtn = document.getElementById("fa-times");
         menuCloseBtn.onclick = function(){
-            "use strict";
             menu.classList.remove("expanded")
         };
         
@@ -76,100 +73,88 @@ var menuCloseBtn = document.getElementById("fa-times");
 
 /*   =====   CHANGE COLORS   =====   */
 
-var settings = document.querySelector(".settings"),
-    blueBtn = document.querySelector(".blue"),
-    redBtn = document.querySelector(".red"),
-    greenBtn = document.querySelector(".green"),
-    grayBtn = document.querySelector(".gray");
+var settings = document.querySelector(".theme-settings"),
+    theme01 = document.querySelector(".theme-01"),
+    theme02 = document.querySelector(".theme-02"),
+    theme03 = document.querySelector(".theme-03"),
+    theme04 = document.querySelector(".theme-04"),
+    themeBtn = document.querySelectorAll(".theme-btn");
 
     
 modeBtn.onclick = function(){
-    "use strict";
     settings.classList.toggle("expanded");
 }
 
-blueBtn.onclick = function(){
-    "use strict";
-    document.body.classList.remove("red-mod", "green-mod", "gray-mod");
-    metaThemeColor.setAttribute("content","#004A6B");
+themeBtn.forEach(element => {
+    element.onclick = changeThemeColor;
+});
 
-    mobBImgLg.style.display = "block";
-    mobRImgLg.style.display = "none";
-    mobGreenImgLg.style.display = "none";
-    mobGrayImgLg.style.display = "none";
-    mobBImgS.style.display = "block";
-    mobRImgS.style.display = "none";
-    mobGreenImgS.style.display = "none";
-    mobGrayImgS.style.display = "none";
+// theme01.onclick = function(){
+//     metaThemeColor.setAttribute("content","#004A6B");
 
+//     mobBImgLg.style.display = "block";
+//     mobRImgLg.style.display = "none";
+//     mobGreenImgLg.style.display = "none";
+//     mobGrayImgLg.style.display = "none";
+//     mobBImgS.style.display = "block";
+//     mobRImgS.style.display = "none";
+//     mobGreenImgS.style.display = "none";
+//     mobGrayImgS.style.display = "none";
+
+// }
+
+// theme02.onclick = function(){
+//     metaThemeColor.setAttribute("content","#830209");
+
+//     mobBImgLg.style.display = "none";
+//     mobRImgLg.style.display = "block";
+//     mobGreenImgLg.style.display = "none";
+//     mobGrayImgLg.style.display = "none";
+//     mobBImgS.style.display = "none";
+//     mobRImgS.style.display = "block";
+//     mobGreenImgS.style.display = "none";
+//     mobGrayImgS.style.display = "none";
+   
+// }
+
+// theme03.onclick = function(){
+//     metaThemeColor.setAttribute("content","#272F19");
+
+//     mobBImgLg.style.display = "none";
+//     mobRImgLg.style.display = "none";
+//     mobGreenImgLg.style.display = "block";
+//     mobGrayImgLg.style.display = "none";
+//     mobBImgS.style.display = "none";
+//     mobRImgS.style.display = "none";
+//     mobGreenImgS.style.display = "block";
+//     mobGrayImgS.style.display = "none";
+
+// }
+
+// theme04.onclick = function(){
+//     metaThemeColor.setAttribute("content","#151821");
+
+//     mobBImgLg.style.display = "none";
+//     mobRImgLg.style.display = "none";
+//     mobGreenImgLg.style.display = "none";
+//     mobGrayImgLg.style.display = "block";
+//     mobBImgS.style.display = "none";
+//     mobRImgS.style.display = "none";
+//     mobGreenImgS.style.display = "none";
+//     mobGrayImgS.style.display = "block";
+
+// }
+
+
+
+//  ===========   FUNCTIONS   =============  //
+
+function changeThemeColor() {
+
+    metaThemeColor.setAttribute("content", this.getAttribute("data-theme"));
+    document.body.classList.remove("theme01-mod", "theme02-mod", "theme03-mod", "theme04-mod");
     document.body.classList.add(this.getAttribute("data-color"));
-    var pageMod = localStorage.setItem("pageColor", this.getAttribute("data-color")),
-        pageHeadTheme = localStorage.setItem("pageTheme", metaThemeColor.getAttribute("content"));
-}
-
-redBtn.onclick = function(){
-    "use strict";
-    document.body.classList.remove("blue-mod", "green-mod", "gray-mod");
-    metaThemeColor.setAttribute("content","#830209");
-
-    mobBImgLg.style.display = "none";
-    mobRImgLg.style.display = "block";
-    mobGreenImgLg.style.display = "none";
-    mobGrayImgLg.style.display = "none";
-    mobBImgS.style.display = "none";
-    mobRImgS.style.display = "block";
-    mobGreenImgS.style.display = "none";
-    mobGrayImgS.style.display = "none";
-
-    document.body.classList.add(this.getAttribute("data-color"));
-    var pageMod = localStorage.setItem("pageColor", this.getAttribute("data-color")),
-        pageHeadTheme = localStorage.setItem("pageTheme", metaThemeColor.getAttribute("content"));
-        
-
-
-
-}
-
-greenBtn.onclick = function(){
-    "use strict";
-    document.body.classList.remove("red-mod", "blue-mod", "gray-mod");
-    metaThemeColor.setAttribute("content","#272F19");
-
-    mobBImgLg.style.display = "none";
-    mobRImgLg.style.display = "none";
-    mobGreenImgLg.style.display = "block";
-    mobGrayImgLg.style.display = "none";
-    mobBImgS.style.display = "none";
-    mobRImgS.style.display = "none";
-    mobGreenImgS.style.display = "block";
-    mobGrayImgS.style.display = "none";
-
-    document.body.classList.add(this.getAttribute("data-color"));
-    var pageMod = localStorage.setItem("pageColor", this.getAttribute("data-color")),
-        pageHeadTheme = localStorage.setItem("pageTheme", metaThemeColor.getAttribute("content"));
-        
-
-
-
-
-}
-
-grayBtn.onclick = function(){
-    "use strict";
-    document.body.classList.remove("blue-mod", "red-mod", "green-mod");
-    metaThemeColor.setAttribute("content","#151821");
-
-    mobBImgLg.style.display = "none";
-    mobRImgLg.style.display = "none";
-    mobGreenImgLg.style.display = "none";
-    mobGrayImgLg.style.display = "block";
-    mobBImgS.style.display = "none";
-    mobRImgS.style.display = "none";
-    mobGreenImgS.style.display = "none";
-    mobGrayImgS.style.display = "block";
-
-    document.body.classList.add(this.getAttribute("data-color"));
-    var pageMod = localStorage.setItem("pageColor", this.getAttribute("data-color")),
-        pageHeadTheme = localStorage.setItem("pageTheme", metaThemeColor.getAttribute("content"));
+    pageMod = localStorage.setItem("pageColor", this.getAttribute("data-color"));
+    pageHeadTheme = localStorage.setItem("pageTheme", metaThemeColor.getAttribute("content"));
 
 }
