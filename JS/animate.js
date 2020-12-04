53895c34d770585c1206436fdb7604c6bd952b0b
MOUSTAPHA98 <67459997+MOUSTAPHA98@users.noreply.github.com>
2020-12-04 18:26:04 +0200

    Update animate.js

--- JS/animate.js
+++ JS/animate.js
@@ -4,54 +4,54 @@ sr.reveal('.animate-left',{
     origin: 'left',
     duration: 750,
     distance: '10rem',
-    delay: 500
+    delay: 1000
 });
 
 sr.reveal('.animate-right',{
     origin: 'right',
     duration: 750,
     distance: '10rem',
-    delay: 500
+    delay: 1000
 });
 
 sr.reveal('.animate-top',{
     origin: 'top',
     duration: 750,
     distance: '10rem',
-    delay: 500
+    delay: 1000
 });
 
 sr.reveal('.animate-bottom',{
     origin: 'bottom',
     duration: 750,
     distance: '10rem',
-    delay: 500
+    delay: 1000
 });
 
 sr.reveal('.animate-left-lazy',{
     origin: 'left',
     duration: 750,
     distance: '10rem',
-    delay: 1500
+    delay: 2000
 });
 
 sr.reveal('.animate-right-lazy',{
     origin: 'right',
     duration: 750,
     distance: '10rem',
-    delay: 1500
+    delay: 2000
 });
 
 sr.reveal('.animate-top-lazy',{
     origin: 'top',
     duration: 750,
     distance: '10rem',
-    delay: 1500
+    delay: 2000
 });
 
 sr.reveal('.animate-bottom-lazy',{
     origin: 'bottom',
     duration: 750,
     distance: '10rem',
-    delay: 1500
+    delay: 2000
 });
